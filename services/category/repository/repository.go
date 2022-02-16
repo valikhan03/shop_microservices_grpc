55f7fcc0fb2f72e5dad1d584a95c0684b3c7d97c
valikhan03 <valikhan.eltaev@gmail.com>
2022-02-16 18:15:58 +0600

    category service demo

--- services/category/repository/repository.go
+++ services/category/repository/repository.go
@@ -7,8 +7,8 @@ import(
 
 type Repository interface{
 	CreateCategory(ctx context.Context, parent_id string, name string, path string) (pb.Category, error)
-	UpdateCategory(ctx context.Context, category pb.Category) (int32, error)
+	UpdateCategory(ctx context.Context, slug string, parent_id string, path string, name string, status int32) (int32, error)
 	DeleteCategory(ctx context.Context, slug string) (int32, error)
 	GetCategory(ctx context.Context, slug string) (pb.Category, error)
-	FindCategory(ctx context.Context, slug string) ([]pb.Category, error)
+	FindCategory(ctx context.Context, slug string) ([]*pb.Category, error)
 }
---
+++ services/category/service/category_service.go
@@ -0,0 +1,59 @@
+package service
+
+import(
+	"context"
+
+	"category_service/pb"
+	"category_service/repository"
+	
+)
+
+type CategoryService struct{
+	repository repository.Repository
+}
+
+func NewCategoryService(r repository.Repository) *CategoryService{
+	return &CategoryService{
+		repository: r,
+	}
+}
+
+func (s *CategoryService) CreateCategory(ctx context.Context, req *pb.CreateCategoryRequest) (*pb.CreateCategoryResponse, error){
+	category, err := s.repository.CreateCategory(ctx, req.ParentId, req.Name, req.Path)
+	res := &pb.CreateCategoryResponse{
+		Category: &category,
+	}
+	return res, err
+}
+
+func (s *CategoryService) UpdateCategory(ctx context.Context, req *pb.UpdateCategoryRequest) (*pb.UpdateCategoryResponse, error){
+	status, err := s.repository.UpdateCategory(ctx, req.GetSlug(), req.GetParentId(), req.GetPath(), req.GetName(), req.GetStatus())
+	res := &pb.UpdateCategoryResponse{
+		Status: status,
+	}
+	return res, err
+}
+
+func (s *CategoryService) DeleteCategory(ctx context.Context, req *pb.DeleteCategoryRequest) (*pb.DeleteCategoryResponse, error){
+	status, err := s.repository.DeleteCategory(ctx, req.GetSlug())
+	res := &pb.DeleteCategoryResponse{
+		Status: status,
+	}
+	return res, err
+}
+
+func (s *CategoryService) GetCategory(ctx context.Context, req *pb.GetCategoryRequest) (*pb.GetCategoryResponse, error){
+	category, err := s.repository.GetCategory(ctx, req.GetSlug())
+	res := &pb.GetCategoryResponse{
+		Category: &category,
+	}
+	return res, err
+}
+
+func (s *CategoryService) FindCategory(ctx context.Context, req *pb.FindCategoryRequest) (*pb.FindCategoryResponse, error){
+	categories, err := s.repository.FindCategory(ctx, req.GetSlug())
+	res := &pb.FindCategoryResponse{
+		Category: categories,
+	}
+	return res, err
+}
